e8022a614aa117efa1a5cb12f36eca3bb39e20d6
AbhishekDoshi26 <adoshi26.ad@gmail.com>
2020-03-26 21:51:15 +0530

    v1.1

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,16 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
+
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+    'Thank You for playing!!',
+  ];
+
+  int qno = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -39,7 +49,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[qno],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -64,12 +74,8 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  scoreKeeper.add(
-                    Icon(
-                      Icons.check,
-                      color: Colors.green,
-                    ),
-                  );
+                  qno++;
+                  if (qno == 4) qno = 3;
                 });
               },
             ),
@@ -89,12 +95,8 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  scoreKeeper.add(
-                    Icon(
-                      Icons.close,
-                      color: Colors.red,
-                    ),
-                  );
+                  qno++;
+                  if (qno == 4) qno = 3;
                 });
               },
             ),
@@ -109,7 +111,5 @@ class _QuizPageState extends State<QuizPage> {
 }
 
 /*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
+question1:
 */
